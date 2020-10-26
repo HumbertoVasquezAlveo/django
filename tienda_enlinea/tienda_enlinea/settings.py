f6872f3e11a5f599c401b81badcbcf9a4ae1703c
Humberto Vasquez <humbertovasquezalveo@gmail.com>
2020-10-26 14:23:02 -0500

    finalizado clase - Relacion muchos a muchos

--- tienda_enlinea/tienda_enlinea/settings.py
+++ tienda_enlinea/tienda_enlinea/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'products',  #--> le indicamos a python que podemos hacer uso de la aplicacion Products
+    'categories',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
